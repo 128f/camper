541b5fdf7c58505bde7258c387fc37d797c50108
Christian Burke <cr0ax64@gmail.com>
2023-05-03 20:54:06 -0700

    Remove transparent background and some other out-dated options

--- nix/nvim/init.lua
+++ nix/nvim/init.lua
@@ -10,17 +10,24 @@ require'nvim-treesitter.configs'.setup {
 	}
 }
 
+local hour = tonumber(os.date('%H'))
+if hour < 19 then
+    vim.cmd('set bg=light')
+else
+    vim.cmd('set bg=dark')
+end
+
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     background = { -- :h background
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = true,
+    transparent_background = false,
     show_end_of_buffer = false, -- show the '~' characters after the end of buffers
     term_colors = false,
     dim_inactive = {
-        enabled = true,
+        enabled = false,
         shade = "dark",
         percentage = 0.15,
     },
